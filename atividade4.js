714a22d7e2a7666e24705024f61caa5901b73d68
JonesPedD <pedjoness28@gmail.com>
2024-09-03 01:24:45 -0300

    atividade 4 feita

---
+++ atividade4.js
@@ -0,0 +1,30 @@
+// fiz o código para justificar a resposta dos que consegui
+
+// 4) Descubra a lógica e complete o próximo elemento:
+
+// a) 1, 3, 5, 7, _9_ = numeros ímpares
+// for (let i = 1; i <= 7;) {
+//     i += 2
+//     console.log(i);
+// }                           
+
+// b) 2, 4, 8, 16, 32, 64, _128_ = sempre multiplicando por 2
+// for (let i = 2; i <= 64;) {
+//     i *= 2;
+//     console.log(i);
+// }
+
+// c) 0, 1, 4, 9, 16, 25, 36, ____ = soma o resultado a um numero primo (1 + 3, 4 + 5, 9 + 7...)
+// let contador = 1;
+// for (let i = 3; contador <= 36; i = i + 2) {
+//     if (i % 2 != 0) {
+//         contador += i;
+//     }
+//     console.log(contador)
+// }
+
+// d) 4, 16, 36, 64, ____ = sequencia de raízes exatas
+
+// e) 1, 1, 2, 3, 5, 8, _13_ = fibonacci, exercício 1
+
+// f) 2,10, 12, 16, 17, 18, 19, _200_ = números que começam com a letra D
