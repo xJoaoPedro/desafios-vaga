c6be50fde2d7dd8b180aef5ef4af5d066cc4ea10
JonesPedD <pedjoness28@gmail.com>
2024-09-03 00:25:17 -0300

    atividade 2 pronta

---
+++ atividade2.js
@@ -0,0 +1,11 @@
+// 2) Escreva um programa que verifique, em uma string, a existência da letra ‘a’, seja maiúscula ou minúscula, além de informar a quantidade de vezes em que ela ocorre.
+// IMPORTANTE: Essa string pode ser informada através de qualquer entrada de sua preferência ou pode ser previamente definida no código;
+
+let palavraVerificada = "Arara";
+let contador = 0;
+
+for (let i = 0; i  < palavraVerificada.length; i++) {
+    if (palavraVerificada[i] == 'a' || palavraVerificada[i] == 'A') { contador ++ }
+}
+
+console.log(`Quantidade de letras a na palavra ${palavraVerificada}: ${contador}`);
